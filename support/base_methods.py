ba078d632221408e2e652b3a0fc5e1a5a71d993b
AOl <aleksanderoleksenko@gmail.com>
2024-02-25 13:04:33 -0400

    Optimized base methods

--- support/base_methods.py
+++ support/base_methods.py
@@ -6,34 +6,27 @@ from selenium.webdriver.support import expected_conditions as EC
 class BaseMethods:
     def __init__(self, driver):
         self.driver = driver
+        self.wait_timeout = 30
 
     def find(self, xpath):
         return self.driver.find_element(By.XPATH, xpath)
 
+    def wait_until(self, condition, xpath, attribute=None, value=None):
+        wait = WebDriverWait(self.driver, self.wait_timeout)
+        locator = (By.XPATH, xpath)
+
+        if attribute and value:
+            wait.until(condition(locator, attribute, value))
+        else:
+            wait.until(condition(locator))
+
     def wait_until_element_displayed(self, xpath):
-        wait = WebDriverWait(self.driver, 30)
-        wait.until(
-            EC.presence_of_element_located(
-                (By.XPATH, xpath)
-            )
-        )
+        self.wait_until(EC.presence_of_element_located, xpath)
 
     def wait_until_element_clickable(self, xpath):
-        wait = WebDriverWait(self.driver, 30)
-        wait.until(
-            EC.element_to_be_clickable(
-                (By.XPATH, xpath)
-            )
-        )
+        self.wait_until(EC.element_to_be_clickable, xpath)
 
     def wait_until_element_has_attribute(self, xpath, attribute, value):
-        wait = WebDriverWait(self.driver, 30)
         # element = f"{xpath}[(contains(@{attribute},'{value}'))]"
         # wait.until(EC.presence_of_element_located((By.XPATH, element)))
-        wait.until(
-            EC.text_to_be_present_in_element_attribute(
-                (By.XPATH, xpath),
-                attribute,
-                value
-            )
-        )
+        self.wait_until(EC.text_to_be_present_in_element_attribute, xpath, attribute, value)
